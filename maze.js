cf8e0a90471f09cb260828951bcedfd190c9fbdd
TekincanCakal <chasenfreelife@gmail.com>
2021-03-27 16:10:19 +0300

    Update

--- maze.js
+++ maze.js
@@ -1,14 +1,120 @@
-class Maze {
-  constructor(Size, CanvasSize) {
+class Maze 
+{
+  constructor(Size, CanvasSize) 
+  {
     this.Size = Size;
     this.BoxSize = CanvasSize / this.Size;
     this.Grid = [];
     this.Stack = [];
-    for (let y = 0; y < this.Size; y++) {
-      for (let x = 0; x < this.Size; x++) {
-        this.Grid.push(new Cell(x, y));
+    for (let y = 0; y < this.Size; y++) 
+    {
+      for (let x = 0; x < this.Size; x++) 
+      {
+        this.Grid.push({X: x, Y: y, Walls: [true, true, true, true], Visited: false});
       }
     }
-    this.Current = this.Grid[0];
+    this.CurrentCell = this.Grid[0];
+  }
+  index(x, y) 
+  {
+    if (x < 0 || y < 0 || x > this.Size - 1 || y > this.Size - 1) 
+    {
+        return -1;
+    }
+    return x + (y* this.Size);
+  }
+  showCell(cell)
+  {
+    let x = cell.X * this.BoxSize;
+    let y = cell.Y * this.BoxSize;
+    stroke(255);
+    if (cell.Walls[0]) 
+    {
+      line(x, y, x + this.BoxSize, y);
+    }
+    if (cell.Walls[1]) 
+    {
+      line(x + this.BoxSize, y, x + this.BoxSize, y + this.BoxSize);
+    }
+    if (cell.Walls[2]) 
+    {
+      line(x + this.BoxSize, y + this.BoxSize, x, y + this.BoxSize);
+    }
+    if (cell.Walls[3]) 
+    {
+      line(x, y + this.BoxSize, x, y);
+    }
+    if (cell.Visited) 
+    {
+      noStroke();
+      fill(255, 0, 255, 100);
+      rect(x, y, this.BoxSize, this.BoxSize);
+    }
+  }
+  showCurrentCell()
+  {
+    let x = this.CurrentCell.X * this.BoxSize;
+    let y = this.CurrentCell.Y * this.BoxSize;
+    noStroke();
+    fill(0, 255, 0, 150);
+    rect(x, y, this.BoxSize, this.BoxSize);
+  }
+  checkNeighbors(cell)
+  {
+    var neighbors = [];
+    var Top = this.Grid[this.index(cell.X, cell.Y - 1)];
+    var Right = this.Grid[this.index(cell.X + 1, cell.Y)];
+    var Bottom = this.Grid[this.index(cell.X, cell.Y + 1)];
+    var Left = this.Grid[this.index(cell.X - 1, cell.Y)];
+    if (Top && !Top.Visited) 
+    {
+      neighbors.push(Top);
+    }
+    if (Right && !Right.Visited) 
+    {
+      neighbors.push(Right);
+    }
+    if (Bottom && !Bottom.Visited) 
+    {
+      neighbors.push(Bottom);
+    }
+    if (Left && !Left.Visited) 
+    {
+      neighbors.push(Left);
+    }
+    if (neighbors.length > 0) 
+    {
+      let r = floor(random(0, neighbors.length));
+      return neighbors[r];
+    } 
+    else 
+    {
+      return undefined;
+    }
+  }
+  removeWalls(next) 
+  {
+    let x = this.CurrentCell.X - next.X;
+    if (x === 1) 
+    {
+      this.CurrentCell.Walls[3] = false;
+      next.Walls[1] = false;
+    } 
+    else if (x === -1) 
+    {
+      this.CurrentCell.Walls[1] = false;
+      next.Walls[3] = false;
+    }
+    let y = this.CurrentCell.Y - next.Y;
+    if (y === 1) 
+    {
+      this.CurrentCell.Walls[0] = false;
+      next.Walls[2] = false;
+    } 
+    else if (y === -1)
+    {
+      this.CurrentCell.Walls[2] = false;
+      next.Walls[0] = false;
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -10,7 +10,9 @@ var MapSize;
 var MapCreateButton;
 var StartButton;
 var SaveButton;
-function setup() {
+
+function setup() 
+{
     createCanvas(CanvasSize, CanvasSize);
     Start = false;
     Slider = document.getElementById("frameRateSlider");
@@ -21,11 +23,95 @@ function setup() {
     SaveButton = document.getElementById("saveButton");
     MapCreateButton = document.getElementById("mapCreateButton");
     SliderText.innerHTML = "FrameRate: " + this.Slider.value
-    Slider.oninput = function () {
+    Slider.oninput = function () 
+    {
         SliderText.innerHTML = "FrameRate: " + this.value;
     }
 }
-function mapCreate() {
+
+function draw() 
+{
+    frameRate(parseInt(Slider.value));
+    background(51);
+    if (this.MazeGame) 
+    {
+        this.MazeGame.Grid.forEach(cell => 
+        {
+            this.MazeGame.showCell(cell);
+        });
+        if (Start) 
+        {
+            if (frameCount % 60 == 0) 
+            {
+                Timer[0]++;
+                if (Timer[0] == 60) 
+                {
+                    Timer[0] = 0;
+                    Timer[1]++;
+                }
+                if (Timer[1] == 60) 
+                {
+                    Timer[1] = 0;
+                    Timer[2]++;
+                }
+                let temp = "Time Eleapsed: ";
+                if (Timer[2] <= 9) 
+                {
+                    temp = temp.concat("0" + Timer[2] + ":");
+                }
+                else 
+                {
+                    temp = temp.concat(Timer[2] + ":");
+                }
+                if (Timer[1] <= 9) 
+                {
+                    temp = temp.concat("0" + Timer[1] + ":");
+                }
+                else 
+                {
+                    temp = temp.concat(Timer[1] + ":");
+                }
+                if (Timer[0] <= 9) 
+                {
+                    temp = temp.concat("0" + Timer[0]);
+                }
+                else 
+                {
+                    temp = temp.concat(Timer[0]);
+                }
+                TimerText.innerHTML = temp;
+            }
+            this.MazeGame.CurrentCell.Visited = true;
+            this.MazeGame.showCurrentCell();
+            var next = this.MazeGame.checkNeighbors(this.MazeGame.CurrentCell);
+            if (next) 
+            {
+                next.Visited = true;
+                this.MazeGame.Stack.push(this.MazeGame.CurrentCell);
+                this.MazeGame.removeWalls(next);
+                this.MazeGame.CurrentCell = next;
+            }
+            else if (this.MazeGame.Stack.length > 0) 
+            {
+                this.MazeGame.CurrentCell = this.MazeGame.Stack.pop();
+            }
+            if (!next && this.MazeGame.Stack.length == 0) 
+            {
+                Start = false;
+                MapCreateButton.disabled = false;
+                StartButton.disabled = true;
+                SaveButton.disabled = false;
+                this.MazeGame.Grid.forEach(cell => 
+                {
+                    cell.Visited = false;
+                });
+            }
+        }
+    }
+}
+
+function mapCreate() 
+{
     MazeGame = new Maze(parseInt(MapSize.value), CanvasSize);
     Timer = [0, 0, 0];
     Start = false;
@@ -33,118 +119,38 @@ function mapCreate() {
     StartButton.disabled = false;
     SaveButton.disabled = true;
 }
-function saveMap(){
+
+function saveMap() 
+{
     let temp = JSON.stringify(this.MazeGame.Grid);
     downloadString(temp, "text/txt", "Map.txt");
 }
-function startStop() {
+
+function startStop() 
+{
     Start = !Start;
-    if (Start) {
+    if (Start) 
+    {
         StartButton.innerHTML = "Stop"
         MapCreateButton.disabled = true;
     }
-    else {
+    else 
+    {
         StartButton.innerHTML = "Start";
         MapCreateButton.disabled = false;
     }
 }
-function draw() {
-    frameRate(parseInt(Slider.value));
-    background(51);
-    if(this.MazeGame){
-        this.MazeGame.Grid.forEach(cell => {
-            cell.show(this.MazeGame.BoxSize);
-        })
-        if (Start) {
-        if (frameCount % 60 == 0) {
-            Timer[0]++;
-            if (Timer[0] == 60) {
-                Timer[0] = 0;
-                Timer[1]++;
-            }
-            if (Timer[1] == 60) {
-                Timer[1] = 0;
-                Timer[2]++;
-            }
-            let temp = "Time Eleapsed: ";
-            if (Timer[2] <= 9) {
-                temp = temp.concat("0" + Timer[2] + ":");
-            }
-            else {
-                temp = temp.concat(Timer[2] + ":");
-            }
-            if (Timer[1] <= 9) {
-                temp = temp.concat("0" + Timer[1] + ":");
-            }
-            else {
-                temp = temp.concat(Timer[1] + ":");
-            }
-            if (Timer[0] <= 9) {
-                temp = temp.concat("0" + Timer[0]);
-            }
-            else {
-                temp = temp.concat(Timer[0]);
-            }
-            TimerText.innerHTML = temp;
-        }
-        this.MazeGame.Current.Visited = true;
-        this.MazeGame.Current.highlight(this.MazeGame.BoxSize);
-        var next = this.MazeGame.Current.checkNeighbors(this.MazeGame.Grid, this.MazeGame.Size);
-        if (next) {
-            next.Visited = true;
-            this.MazeGame.Stack.push(this.MazeGame.Current);
-            removeWalls(this.MazeGame.Current, next);
-            this.MazeGame.Current = next;
-        }
-        else if (this.MazeGame.Stack.length > 0) {
-            this.MazeGame.Current = this.MazeGame.Stack.pop();
-        }
-        if(!next && this.MazeGame.Stack.length == 0){
-            Start = false;
-            MapCreateButton.disabled = false;
-            StartButton.disabled = true;
-            SaveButton.disabled = false;
-            this.MazeGame.Grid.forEach(cell => {
-                cell.Visited = false;
-            })
-        }
-    }
-    }
-}
 
-function index(x, y, size) {
-    if (x < 0 || y < 0 || x > size - 1 || y > size - 1) {
-        return -1;
-    }
-    return x + y * size;
-}
-function removeWalls(current, next) {
-    let x = current.X - next.X;
-    if (x === 1) {
-        current.Walls[3] = false;
-        next.Walls[1] = false;
-    } else if (x === -1) {
-        current.Walls[1] = false;
-        next.Walls[3] = false;
-    }
-    let y = current.Y - next.Y;
-    if (y === 1) {
-        current.Walls[0] = false;
-        next.Walls[2] = false;
-    } else if (y === -1) {
-        current.Walls[2] = false;
-        next.Walls[0] = false;
-    }
-}
-function downloadString(text, fileType, fileName) {
-  var blob = new Blob([text], { type: fileType });
-  var a = document.createElement('a');
-  a.download = fileName;
-  a.href = URL.createObjectURL(blob);
-  a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
-  a.style.display = "none";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
+function downloadString(text, fileType, fileName) 
+{
+    var blob = new Blob([text], { type: fileType });
+    var a = document.createElement('a');
+    a.download = fileName;
+    a.href = URL.createObjectURL(blob);
+    a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    setTimeout(function () { URL.revokeObjectURL(a.href); }, 1500);
 }
